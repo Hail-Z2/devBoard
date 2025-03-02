60bf3b652a43239576535574b40cc547f7367c2f
Hail-Z2 <suhailsaiful22@gmail.com>
2025-03-02 23:03:27 +0600

    Added Blog html and its js file

---
+++ Scripts/blog.js
@@ -0,0 +1,5 @@
+const backBtn = document.getElementById('btn-back');
+
+backBtn.addEventListener('click',function(){
+  window.location.href="./index.html"
+})
---
+++ Scripts/dev.js
@@ -0,0 +1,6 @@
+const devBoard = document.getElementById('devThings');
+
+devBoard.addEventListener('click',function(){
+  window.location.href="./blog.html"
+})
+
