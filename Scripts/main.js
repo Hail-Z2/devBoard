a56bf61f012867bf212555c3972926d7118fd7d9
Hail-Z2 <suhailsaiful22@gmail.com>
2025-03-02 23:49:14 +0600

    Final COmmit

--- Scripts/main.js
+++ Scripts/main.js
@@ -6,8 +6,30 @@ const activityLog= document.getElementById('activity');
 const clearHistory = document.getElementById('history');
 
 
+
 let taskRemaining= 6;
 let taskComplete=23;
+
+const colors = [
+  "#7A73D1", 
+  "#B4EBE6", 
+  "#D5C7A3", 
+  "#D3E671", 
+  "#E53888", 
+  "#7D1C4A",
+  "#2D336B", 
+];
+
+function randomColour(){
+  const random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+
+backgroundColour.addEventListener('click',function(){
+  const random=randomColour();
+  document.body.style.backgroundColor=random
+
+});
 
 
 for(let btn=0; btn<buttonsComplete.length; btn++){
@@ -31,6 +53,11 @@ for(let btn=0; btn<buttonsComplete.length; btn++){
       const taskName = buttonsComplete[btn].parentElement.parentElement.querySelector("p.font-medium.text-2xl").innerHTML;
     const timeStamp = new Date().toLocaleString();
     activityLog.innerHTML += `<p>${taskName} completed at ${timeStamp}</p>`;
+
+    if(taskRemaining===0){
+      alert("Tasks have been completed")
+    }
+    
     
   })
 
@@ -39,3 +66,4 @@ for(let btn=0; btn<buttonsComplete.length; btn++){
 clearHistory.addEventListener('click',function(){
   activityLog.innerHTML=""
 })
+
